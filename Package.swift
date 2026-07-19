5eb973c11d8d7966fbe843259b90a79dec569b72
Michael Biehler <132991600+biehlermi@users.noreply.github.com>
2026-07-20 00:58:20 +0200

    fix: Downgrade swift-tools-version to 6.3
    This is required for compatibility reasons (for dependabot, code-ql, etc.)

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 6.4
+// swift-tools-version: 6.3
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
   name: "MaritimeRouteKit",
   platforms: [
-    .iOS(.v27)
+    .iOS(.v26)
   ],
   products: [
     // Products define the executables and libraries a package produces, making them visible to other packages.
